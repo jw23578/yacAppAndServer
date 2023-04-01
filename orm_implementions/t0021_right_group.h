a0157aa2d1b77157d5440cd781d4acd21a7df6d1
jw23578 <jens@wienoebst.com>
2023-04-01 21:12:43 +0200

    test if Q_OBJECT is really needed

--- orm_implementions/t0021_right_group.h
+++ orm_implementions/t0021_right_group.h
@@ -5,10 +5,6 @@
 
 class t0021_right_group : public YACBaseObject
 {
-#ifdef ORMQTTypes
-    Q_OBJECT
-#endif
-
     MACRO_UUID_PROPERTY(app_id);
     MACRO_STRING_PROPERTY(name);
     MACRO_UUID_PROPERTY(creater_id);
