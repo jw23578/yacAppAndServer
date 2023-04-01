69caec5ebd79046d1311b7942d9510b72e2cbb27
jw23578 <jens@wienoebst.com>
2023-04-02 00:43:00 +0200

    extended factory

--- orm_implementions/ormfactory.cpp
+++ orm_implementions/ormfactory.cpp
@@ -1,7 +1,9 @@
 #include "ormfactory.h"
 #include "t0009_appuser_logintoken.h"
-#include "t0026_appuser_tag.h"
 #include "t0021_right_group.h"
+#include "t0022_right_group2appuser.h"
+#include "t0023_right2rightgroup.h"
+#include "t0026_appuser_tag.h"
 
 
 void ORMFactory::addGhost(YACBaseObject *ghost)
@@ -14,6 +16,8 @@ ORMFactory::ORMFactory()
 {
     addGhost(new t0009_appuser_logintoken);
     addGhost(new t0021_right_group);
+    addGhost(new t0022_right_group2appuser);
+    addGhost(new t0023_right2rightgroup);
     addGhost(new t0026_appuser_tag);
 }
 
