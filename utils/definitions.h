d10a17cef3a77f65e5f7e70cb98b4ac04db2585a
jw23578 <jens@wienoebst.com>
2024-06-06 17:50:05 +0200

    Postgre eingefügt

--- utils/definitions.h
+++ utils/definitions.h
@@ -9,8 +9,8 @@
 #define UNUSED(x) (void)(x)
 
 typedef std::chrono::system_clock::time_point TimePoint;
-const std::chrono::system_clock::time_point TimePointSqlNull = std::chrono::system_clock::time_point::min();
-const std::chrono::system_clock::time_point TimePointSqlNow = std::chrono::system_clock::time_point::min() + std::chrono::microseconds(1);
+const std::chrono::system_clock::time_point TimePointPostgreSqlNull = std::chrono::system_clock::time_point::min();
+const std::chrono::system_clock::time_point TimePointPostgreSqlNow = std::chrono::system_clock::time_point::min() + std::chrono::microseconds(1);
 
 const sole::uuid NullUuid = sole::rebuild("00000000-0000-0000-0000-000000000000");
 
