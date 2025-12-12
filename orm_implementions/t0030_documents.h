c26342dfc422a22975861127265a0ee9be76543b
jw23578 <jens@wienoebst.com>
2025-12-12 21:59:48 +0100

    t0030_documents ist jetzt auch ein AppBaseObject

--- orm_implementions/t0030_documents.h
+++ orm_implementions/t0030_documents.h
@@ -1,18 +1,18 @@
 #ifndef T0030_DOCUMENTS_H
 #define T0030_DOCUMENTS_H
 
-#include "yacbaseobject.h"
+#include "appbaseobject.h"
 
-class t0030_documents : public YACBaseObject
+class t0030_documents : public AppBaseObject
 {
 #ifdef ORMQTTypes
     Q_OBJECT
 #endif
 
+    MACRO_UUID_PROPERTY(document_id);
     MACRO_UUID_PROPERTY(app_id);
     MACRO_UUID_PROPERTY(creater_id);
     MACRO_UUID_PROPERTY(document_blob_id);
-    MACRO_UUID_PROPERTY(document_id);
     MACRO_INT_PROPERTY(document_version);
     MACRO_STRING_PROPERTY(document_name);
     MACRO_STRING_PROPERTY(document_type);
@@ -26,12 +26,12 @@ class t0030_documents : public YACBaseObject
     MACRO_STRING_PROPERTY(transfer_document_base64);
     MACRO_STRING_PROPERTY(transfer_comma_separated_catchphrases_base64);
 public:
-    t0030_documents():YACBaseObject(Rights::RN_everybody)
+    t0030_documents():AppBaseObject(Rights::RN_everybody)
     {
+        MACRO_ADD_PROPERTY_ADD_DETAIL(document_id, DetailID);
         MACRO_ADD_INDEX_PROPERTY(app_id);
         MACRO_ADD_PROPERTY(creater_id);
         MACRO_ADD_PROPERTY(document_blob_id);
-        MACRO_ADD_PROPERTY(document_id);
         MACRO_ADD_PROPERTY(document_version);
         MACRO_ADD_PROPERTY(document_name);
         MACRO_ADD_PROPERTY(document_type);
